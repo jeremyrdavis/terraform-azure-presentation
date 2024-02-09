31f265704e7bedf41784f24e6424ac730ee0edd1
Jeremy Davis <jeremyrdavis@mac.com>
2024-02-09 16:04:30 -0500

    updated with keys

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://learn.microsoft.com/en-us/azure/developer/terraform/create-resource-group?tabs=azure-cli
+
+terraform init
+terraform validate
+terraform plan -out main.tfplan
--- main.tf
+++ main.tf
@@ -5,4 +5,12 @@ resource "random_pet" "rg_name" {
 resource "azurerm_resource_group" "rg" {
     location = var.resource_group_location
     name = random_pet.rg_name.id
+
+    tags = {
+        Environment = "dev"
+        Application = "Hello, World!"
+        Creator     = "Jeremy Davis"
+        Role        = "Friendly Red Hat Presenter"
+    } 
+
 }
